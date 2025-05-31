f2ef789b7d854afa2039c3d01b93df68b9987470
Moseenkov Andrey <moseenkowam@gmail.com>
2025-05-31 23:14:28 +0300

    fix zmei

--- Zmeini_Gorod/output-1/skin.js
+++ Zmeini_Gorod/output-1/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 5.2.4/15996
 // Filename: simplex_v5_autohide.ggsk
-// Generated Сб май 31 23:06:54 2025
+// Generated Сб май 31 23:07:25 2025
 
 function pano2vrSkin(player,base) {
 	var ggSkinVars = [];
