f48f0745ffca3138110f76de6e6ee8d8c5c77b8a
Moseenkov Andrey <moseenkowam@gmail.com>
2025-08-20 20:06:55 +0300

    upd cvet

--- Cvetocny_Gorod/output/skin.js
+++ Cvetocny_Gorod/output/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 5.2.4/15996
 // Filename: simplex_v5_autohide.ggsk
-// Generated Пн июл 28 16:19:48 2025
+// Generated Ср авг 20 20:06:37 2025
 
 function pano2vrSkin(player,base) {
 	var ggSkinVars = [];
